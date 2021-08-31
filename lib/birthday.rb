7adbaca0c4fd34bec65c22db5dedd77289179597
OLGA <olichka23.85@gmail.com>
2021-08-31 11:34:42 -0400

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  puts("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nHappy Birthday Amanda! You are now #{birthday_kids["Amanda"]} years old!")
 end
 
 
